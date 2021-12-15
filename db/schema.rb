455c0b046399ca6378f25dd83604805bef67624c
michelvalenzuela <87387556+michelvalenzuela@users.noreply.github.com>
2021-12-15 16:42:48 -0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_25_014316) do
+ActiveRecord::Schema.define(version: 2021_12_15_014316) do
 
   create_table "owners", force: :cascade do |t|
     t.string "name"
